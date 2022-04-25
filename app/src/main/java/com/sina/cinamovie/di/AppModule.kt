7e00dbd5adbe517dfd3c4b4ed60f3fb70b490c4d
sina kazemi <m.sina.k2018@gmail.com>
2022-04-25 10:02:06 +0430

    Create di

--- app/src/main/java/com/sina/cinamovie/di/AppModule.kt
+++ app/src/main/java/com/sina/cinamovie/di/AppModule.kt
@@ -1,49 +1,58 @@
 package com.sina.cinamovie.di
 
 import android.content.SharedPreferences
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.sina.cinamovie.api.ApiService
+import com.sina.cinamovie.util.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
+
 
 @Module
 @InstallIn(SingletonComponent::class)
 class AppModule {
 
-//    @Singleton
-//    @Provides
-//    fun provideOkHttpClient(
-//        tokenRepo: TokenRepo?,
-//        sharedPreferences: SharedPreferences?
-//    ): OkHttpClient? {
-//        val okHttpClient = Builder()
-//        return okHttpClient.connectTimeout(60, TimeUnit.SECONDS)
-//            .readTimeout(60, TimeUnit.SECONDS)
-//            .writeTimeout(60, TimeUnit.SECONDS)
-//            .authenticator(TokenAuthenticator(tokenRepo))
-//            .addInterceptor(TokenInterceptor(sharedPreferences))
-//            .build()
-//    }
-//
-//    @Singleton
-//    @Provides
-//    fun provideApiService(retrofit: Retrofit): ApiService? {
-//        return retrofit.create(ApiService::class.java)
-//    }
-//
-//    @Singleton
-//    @Provides
-//    fun provideRetrofit(okHttpClient: OkHttpClient?, tools: Tools): Retrofit? {
-//        val gson: Gson = GsonBuilder().setLenient().create()
-//        return Builder()
-//            .addConverterFactory(GsonConverterFactory.create(gson))
-//            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-//            .baseUrl(tools.getBaseUrl())
-//            .client(okHttpClient)
-//            .build()
-//    }
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        sharedPreferences: SharedPreferences?
+    ): OkHttpClient {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+        return OkHttpClient().newBuilder()
+            .addInterceptor(logging)
+            .readTimeout(30 , TimeUnit.SECONDS)
+            .writeTimeout(30 , TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(
+                GsonConverterFactory.create(
+                    GsonBuilder()
+                        .setLenient()
+                        .create()))
+            .client(okHttpClient)
+            .build()
+    }
 
 }
--- app/src/main/java/com/sina/cinamovie/util/Constants.kt
+++ app/src/main/java/com/sina/cinamovie/util/Constants.kt
@@ -2,3 +2,4 @@ package com.sina.cinamovie.util
 
 const val DOWNLOAD_FILES_DIR = "DownloadFiles"
 const val ITEM_ID = "ItemId"
+const val BASE_URL = "BASE_URL"
